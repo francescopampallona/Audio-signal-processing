72e57e67e0aefc0f28af17a41a62e863b6de9d73
Francesco Pampallona <53058502+francescopampallona@users.noreply.github.com>
2019-10-08 21:28:26 +0200

    Add files via upload

---
+++ sound_math_function/smf.py
@@ -0,0 +1,45 @@
+"""
+Created on Tue Oct  8 18:51:43 2019
+
+@author: racen
+"""
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.io.wavfile import write
+import sounddevice as sd
+'''
+DATI SUL CAMPIONAMENTO 
+'''
+DURATA = 4.0 #Durata del suono in secondi
+SF = 1500.0 #FREQUENZA DI CAMPIONAMENTO
+
+'''
+DATI DEL SEGNALE
+'''
+f = 500 #Frequenza del segnale
+w = 2*math.pi*f #Pulsazione del segnale
+t = np.arange(0, DURATA, 1/SF)
+function = np.sin(w*t)
+   
+
+'''
+Grafico del segnale in funzione del tempo
+'''
+print("GRAFICO")
+fig, ax = plt.subplots()
+ax.plot(t, function)
+plt.title('SEGNALE SONORO IN FUNZIONE DEL TEMPO')
+plt.ylabel('SUONO')
+plt.xlabel('TEMPO IN SECONDI')
+#plt.savefig("signal.png")
+ax.grid()
+plt.show()
+'''
+SALVATAGGIO DEL SEGNALE IN WAV
+'''
+print("NUMERO DI CAMPIONI: ",DURATA*SF)
+write('test.wav', int(SF), function)
+sd.play(function, SF)
+
+
