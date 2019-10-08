6f53e631ee9573f1e469c89397d425a26d6656f5
Francesco Pampallona <53058502+francescopampallona@users.noreply.github.com>
2019-10-09 00:01:09 +0200

    Add files via upload

--- sound_math_function/smf.py
+++ sound_math_function/smf.py
@@ -4,10 +4,17 @@ import matplotlib.pyplot as plt
 from scipy.io.wavfile import write
 import sounddevice as sd
 '''
-DATI SUL CAMPIONAMENTO 
+SEGNALE
+'''
+def signal(w, t):
+    signal = np.sin(w*t)*np.sin((w/500)*t)
+    return signal
+    
+'''
+DATI SULLA DURATA DEL SEGNALE E SUL SUO CAMPIONAMENTO 
 '''
 DURATA = 4.0 #Durata del suono in secondi
-SF = 1500.0 #FREQUENZA DI CAMPIONAMENTO
+SF = 10000.0 #FREQUENZA DI CAMPIONAMENTO
 
 '''
 DATI DEL SEGNALE
@@ -15,23 +22,22 @@ DATI DEL SEGNALE
 f = 500 #Frequenza del segnale
 w = 2*math.pi*f #Pulsazione del segnale
 t = np.arange(0, DURATA, 1/SF)
-function = np.sin(w*t)
+function = signal(w, t)
    
 
 '''
 Grafico del segnale in funzione del tempo
 '''
-print("GRAFICO")
 fig, ax = plt.subplots()
 ax.plot(t, function)
 plt.title('SEGNALE SONORO IN FUNZIONE DEL TEMPO')
 plt.ylabel('SUONO')
 plt.xlabel('TEMPO IN SECONDI')
-#plt.savefig("signal.png")
+plt.savefig("signal.png")
 ax.grid()
 plt.show()
 '''
-SALVATAGGIO DEL SEGNALE IN WAV E RIPRODUZIONE DEL SUONO TRAMITE LA LIBRERIA sounddevice
+SALVATAGGIO DEL SEGNALE IN WAV
 '''
 print("NUMERO DI CAMPIONI: ",DURATA*SF)
 write('test.wav', int(SF), function)
