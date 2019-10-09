7c4b1d942028613fd98bef6c911edf997f6ea69b
Francesco Pampallona <53058502+francescopampallona@users.noreply.github.com>
2019-10-09 20:32:07 +0200

    Add files via upload

---
+++ audio_reader/reader.py
@@ -0,0 +1,39 @@
+from scipy.io.wavfile import read
+import sounddevice as sd
+import numpy as np
+import matplotlib.pyplot as plt
+
+'''
+READ AUDIO FROM WAV FILE 
+'''
+data=read("doorbell_x.wav")
+'''
+SAMPLE FREQUENCY
+'''
+sf=data[0]
+print("SAMPLE FREQUENCY: ", sf)
+'''
+SIGNAL DATA
+'''
+sound=np.array(data[1])
+SAMPLES=len(sound)
+DURATA = SAMPLES/sf
+print(SAMPLES, " " , sound.dtype, " DURATA: ", DURATA, " s")
+'''
+GRAFICO DEL SEGNALE AUDIO
+'''
+t = np.arange(0, DURATA, 1/sf)
+fig, ax = plt.subplots()
+ax.plot(t, sound)
+plt.title('SEGNALE SONORO IN FUNZIONE DEL TEMPO')
+plt.ylabel('SUONO')
+plt.xlabel('TEMPO IN SECONDI')
+plt.savefig("signal.png")
+ax.grid()
+plt.show()
+'''
+PLAY AUDIO
+'''
+sd.play(sound, fs)
+
+
